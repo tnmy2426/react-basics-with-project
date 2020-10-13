c0c874bc957784003dc5944423d9f40712e83add
Abdullah Al Nahdi <al.nahdi.tnmy@gmail.com>
2020-10-13 17:07:12 +0600

    Current date or local time rendering in jsx challenge

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+const name = "TanmoY";
+const currentDate = new Date().toLocaleDateString();
+const currentTime = new Date().toLocaleTimeString();
+
+
+const elements = <Fragment>
+  <h1>Hello, My name is {name}</h1>
+  <h3>Date is: {currentDate}</h3>
+  <h4>Time is: {currentTime} </h4>
+</Fragment>
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  elements,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
