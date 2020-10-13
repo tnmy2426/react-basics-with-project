67ce9d8c8f33df071a9d8419be7476a4121c5f39
Abdullah Al Nahdi <al.nahdi.tnmy@gmail.com>
2020-10-13 20:40:32 +0600

    Images practice and JSX attributes using href,src,alt

--- src/index.js
+++ src/index.js
@@ -6,11 +6,21 @@ const name = "TanmoY";
 const currentDate = new Date().toLocaleDateString();
 const currentTime = new Date().toLocaleTimeString();
 
+const img1 = "https://picsum.photos/200/300";
+const img2 = "https://picsum.photos/250/300";
+const img3 = "https://picsum.photos/300/300";
+const img_alt = "randomImages"
+
+const refference = "https://www.google.com";
+
 
 const elements = <Fragment>
   <h1>Hello, My name is {name}</h1>
   <h3>Date is: {currentDate}</h3>
   <h4>Time is: {currentTime} </h4>
+  <img src={img1} alt={img_alt} />
+  <a href={refference}><img src={img2} alt={img_alt} /></a>
+  <img src={img3} alt={img_alt} />
 </Fragment>
 
 ReactDOM.render(
