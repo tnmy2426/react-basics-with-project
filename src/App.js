fd46f95d15a9b1df21f6b8bac027a8f5622ca90e
Abdullah Al Nahdi <al.nahdi.tnmy@gmail.com>
2020-10-25 12:40:43 +0600

    Login form submit practice

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
 import React, { useState } from 'react'
+import './App.css'
 // import { sum, sub, mul, divi } from './Calc';
 
 const App = () => {
 
-    const [count, setCount] = useState(0)
-    const increament = () =>{
-        setCount(count+1)
+    const [inputDataFirstName, setInputDataFirstName] = useState('')
+    const inputHandlerOne = (event) => {
+        setInputDataFirstName(event.target.value)
     }
-    
+
+    const [inputDataLastName, setInputDataLastName] = useState('')
+    const inputHandlerTwo = (event)=>{
+        setInputDataLastName(event.target.value)
+    }
+
+    const [firstName, setFirstName] = useState('')
+    const [lastName, setLastName] = useState('')
+    const btnHandler = (event) => {
+        event.preventDefault();
+        setFirstName(inputDataFirstName);
+        setLastName(inputDataLastName);
+    }
+
     return (
         <div className='container'>
-            <h1>{count}</h1>
-            <button onClick={increament}>Counter</button>
+            <form onSubmit={btnHandler}>
+                <div>
+                    <h1>Hello {firstName} {lastName}</h1>
+                    <input type='text' placeholder='Enter Your First Name' onChange={inputHandlerOne} value={inputDataFirstName} />
+                    <input type='text' placeholder='Enter Your Last Name' onChange={inputHandlerTwo} value={inputDataLastName} />
+                    <button type='submit'>Submit 👍 </button>
+                </div>
+            </form>
         </div>
     )
 }
